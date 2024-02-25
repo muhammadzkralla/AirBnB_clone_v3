4605a8eb5485391ac8015dbd088827e2d69d00dc
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-26 00:47:30 +0200

    all views

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-
-from flask import Flask
-from api.v1.views import app_views
+"""
+start listening
+"""
 from models import storage
+from flask import Flask, jsonify
+from flask_cors import CORS
+from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Teardown app context"""
+def tear_down(self):
+    """closes the storage"""
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """handles 404 error"""
+    return (jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.register_blueprint(app_views, url_prefix="/api/v1")
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
-
+"""
+Start listening
+"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    """Returns the status of the API"""
-    return jsonify({"status": "OK"})
+
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """respond with OK for /status"""
+    j_status = {"status": "OK"}
+    return jsonify(j_status)
+
+
+@app_views.route("/stats", strict_slashes=False)
+def count_all():
+    """respond with OK for /stats"""
+    return jsonify({
+        name: storage.count(obj) for name, obj in classes.items()
+    })
