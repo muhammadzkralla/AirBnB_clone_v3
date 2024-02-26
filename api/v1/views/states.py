11702ebedcaafcd00fc3e4e56cf32bbca23778d6
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-27 01:32:40 +0200

    pycode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -51,6 +51,7 @@ def create_state():
     state.save()
     return jsonify(state.to_dict()), 201
 
+
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """PUT state"""
