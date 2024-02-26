d86212dbe7709c2f461b57b0fd3f0ef7fb89957a
Zkrallah <muhammad.heshamyt@gmail.com>
2024-02-27 00:31:58 +0200

    Revert "places"
    This reverts commit f6000e1a7a2427d4e270f246ef142acba8ea4a01.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-'''
+"""
 start listening
-'''
-
-from os import getenv
+"""
+from models import storage
 from flask import Flask, jsonify
 from flask_cors import CORS
-from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
-
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_engine(exception):
+def tear_down(self):
     """closes the storage"""
     storage.close()
 
@@ -26,11 +23,10 @@ def teardown_engine(exception):
 @app.errorhandler(404)
 def not_found(error):
     """handles 404 error"""
-    response = {'error': 'Not found'}
-    return jsonify(response), 404
+    return (jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-'''
+"""
 Start listening
-'''
-
-from flask import jsonify
+"""
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    '''
-    Return OK
-    '''
-    response = {'status': 'OK'}
-    return jsonify(response)
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """respond with OK for /status"""
+    j_status = {"status": "OK"}
+    return jsonify(j_status)
 
 
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    '''
-    GET stats
-    '''
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(stats)
+@app_views.route("/stats", strict_slashes=False)
+def count_all():
+    """respond with OK for /stats"""
+    return jsonify({
+        name: storage.count(obj) for name, obj in classes.items()
+    })
